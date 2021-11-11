b7ff4420dad379a901ee08b18361c5dd5a294e06
Kate Meuse <katemeuse@gmail.com>
2021-11-10 23:48:30 -0500

    make first call to db from api

--- bin/app.ml
+++ bin/app.ml
@@ -1,4 +1,5 @@
 open Opium
+open Lwt.Syntax
 
 module Person = struct
   type t = {
@@ -8,10 +9,6 @@ module Person = struct
 
   let yojson_of_t t =
     `Assoc [ ("name", `String t.content); ("id", `Int t.id) ]
-
-  (* let t_of_yojson yojson = match yojson with | `Assoc [ ("name",
-     `String name); ("age", `Int age) ] -> { name; age } | _ -> failwith
-     "invalid person json" *)
 end
 
 let print_person_handler req =
@@ -21,14 +18,7 @@ let print_person_handler req =
   let person = { Person.content; id } |> Person.yojson_of_t in
   Lwt.return (Response.of_json person)
 
-let unwrap call =
-  let result =
-    match Lwt.state call with
-    | Sleep -> failwith "sleeping?"
-    | Return x -> x
-    | Fail exn -> raise exn
-  in
-  match result with
+let unwrap = function
   | Ok x -> x
   | Error (Db.Database_error exn) -> failwith exn
 
@@ -36,8 +26,8 @@ let get_all_db db = Lwt.return db
 
 let print_first_todo req =
   let id = Router.param req "id" |> int_of_string in
-  let todos = unwrap (Lwt.bind (Db.get_all ()) get_all_db) in
-  let one = List.hd todos in
+  let* todos = Db.get_all () in
+  let one = List.hd (unwrap todos) in
   let person =
     { Person.content = one.content; Person.id } |> Person.yojson_of_t
   in
