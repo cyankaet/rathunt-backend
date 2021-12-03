75665faf16b30fd98cbebf7d55d7a46911e0ac22
Kate Meuse <katemeuse@gmail.com>
2021-12-03 01:39:37 -0500

    working post test

--- bin/app.ml
+++ bin/app.ml
@@ -1,4 +1,5 @@
 open Opium
+open Opium.Request
 open Lwt.Syntax
 open Database
 open Database.Types
@@ -9,6 +10,12 @@ open Database.Types
 let unwrap = function
   | Ok x -> x
   | Error (Db.Database_error exn) -> failwith exn
+
+let read_form_data req =
+  let* req = to_multipart_form_data req in
+  match req with
+  | None -> failwith "no form data"
+  | Some lst -> Lwt.return lst
 
 (* litmus test for whether the API is working at all, returns the inputs
    you passed and solves = 0 *)
@@ -33,9 +40,17 @@ let get_all_teams _ =
   let team_lst = unwrap teams |> serialize_teams in
   Lwt.return (Response.of_json (`List team_lst))
 
+let test_post req =
+  let* req = read_form_data req in
+  let name = List.assoc "team" req in
+  let solves = List.assoc "solves" req |> int_of_string in
+  let team = { Team.name; id = 3; Team.solves } |> Team.yojson_of_t in
+  Lwt.return (Response.of_json team)
+
 let _ =
   App.empty
   |> App.get "/team/:id/:name" print_team_handler
   |> App.get "/team/first" get_first_team
   |> App.get "/teams/" get_all_teams
+  |> App.post "/team/post/" test_post
   |> App.run_command
